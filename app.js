31d377a60b4fbb828478256e634c3909810e8bd9
Gabe Alena <slabsk@mail.ru>
2022-06-14 17:45:18 +0500

    changes7

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
-app.use((req, _res, next) => {
+app.use((req, res, next) => {
   req.user = {
     _id: '62a766fdc4a74e048b6f8233',
   };
 
   next();
 });
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
