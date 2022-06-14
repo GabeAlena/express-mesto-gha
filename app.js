f39ec3eb4fb1ea0fdcb90102bb19bd5ea717e456
Gabe Alena <slabsk@mail.ru>
2022-06-14 22:16:52 +0500

    last changes Project 13

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемая страница не найдена' });
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 app.listen(PORT);
