5014316a8dad4d7c6feda948b37874e12826fa6e
Gabe Alena <slabsk@mail.ru>
2022-06-26 11:19:17 +0500

    Решила проблему авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,21 @@
 const jwt = require('jsonwebtoken');
+const { Unauthorized } = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const authorization = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация!' });
+  if (!authorization) {
+    throw new Unauthorized('Необходима авторизация!');
   }
 
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(authorization, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   req.user = payload;
-
   return next();
 };
