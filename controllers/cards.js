57494c3d80f5f5ca5fbe99bcfdd29f7973acbb7a
Gabe Alena <slabsk@mail.ru>
2022-06-27 09:51:44 +0500

    cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,15 +1,17 @@
 const Card = require('../models/card');
-const { ERROR_CODE, NOT_FOUND, ERROR_DEFAULT } = require('../utils/utils');
+
+const ValidationError = require('../errors/ValidationError');
+const NotFound = require('../errors/NotFound');
 
 /* возвращает все карточки */
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' }));
+    .catch((err) => next(err));
 };
 
 /* создает карточку */
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   // console.log(req.user._id); // _id станет доступен
   const { name, link } = req.body;
   const owner = req.user._id;
@@ -18,59 +20,91 @@ module.exports.createCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE).send({ message: `Данные некорректны ${err.message}` });
+        return next(new ValidationError(`Данные некорректны ${err.message}`));
       }
-      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' });
-    });
+      return next(err);
+    })
+    .catch(next);
 };
 
 /* удаляет карточку по идентификатору */
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        throw new NotFound('Запрашиваемая карточка не найдена');
+        /* return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' }); */
       }
       return res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE).send({ message: 'Некорректные данные. Проверьте id карточки' });
+      if (err.name === 'ValidationError') {
+        next(new ValidationError(`Данные некорректны ${err.message}`));
+        return;
       }
-      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' });
+      next(err);
+
+    /* .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Некорректные данные. Проверьте id карточки'
+        });
+      }
+      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' }); */
     });
 };
 
 /* поставить лайк карточке */
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        throw new NotFound('Запрашиваемая карточка не найдена');
+        /* return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' }); */
       }
       return res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE).send({ message: 'Некорректные данные. Проверьте id карточки' });
+      if (err.name === 'ValidationError') {
+        next(new ValidationError(`Данные некорректны ${err.message}`));
+        return;
       }
-      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' });
+      next(err);
+
+    /* .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Некорректные данные. Проверьте id карточки'
+        });
+      }
+      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' }); */
     });
 };
 
 /* убрать лайк с карточки */
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        throw new NotFound('Запрашиваемая карточка не найдена');
+
+        /* return res.status(NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' }); */
       }
       return res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE).send({ message: 'Некорректные данные. Проверьте id карточки' });
+      if (err.name === 'ValidationError') {
+        next(new ValidationError(`Данные некорректны ${err.message}`));
+        return;
       }
-      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' });
+      next(err);
+
+    /* .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({
+          message: 'Некорректные данные. Проверьте id карточки'
+        });
+      }
+      return res.status(ERROR_DEFAULT).send({ message: 'Сервер не может обработать запрос' }); */
     });
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const cardRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getCards,
   createCard,
@@ -8,7 +10,12 @@ const {
 } = require('../controllers/cards');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
+cardRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(/^https?:\/\/(www\.)?[a-zA-Z\0-9]+\.[\w\d\-._~:/?#[\]@!$&'()*+,;=]{2,}#?/),
+  }),
+}), createCard);
 cardRouter.delete('/:cardId', deleteCard);
 cardRouter.put('/:cardId/likes', likeCard);
 cardRouter.delete('/:cardId/likes', dislikeCard);
